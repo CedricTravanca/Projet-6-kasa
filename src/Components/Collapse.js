89b5c4608a96c32d737a825ed1535e521f382485
Cedric Travanca <cedric.travanca@gmail.com>
2024-03-14 15:58:22 +0100

    fleches de about fonctionnelles et card avec dégradé

--- src/Components/Collapse.js
+++ src/Components/Collapse.js
@@ -3,12 +3,10 @@ import arrow from "../Assets/arrow.png"
 
 function Collapse({ title, content }) {
  const [isOpen, setIsOpen] = useState(false);
- //const [isRotated, setIsRotated] = useState(false);
- //{isRotated && <div className=".collapse-arrow"></div>}
 
  return (
     <div>
-      <button onClick={() => setIsOpen(!isOpen)}>{title}<img src={arrow} alt='Fleche pour montrer le contenu' className='collapse-arrow'></img></button>
+      <button>{title}<img onClick={() => setIsOpen(!isOpen)}src={arrow} alt='Fleche pour montrer le contenu' className={isOpen ? "open-arrow" : "close-arrow"}></img></button>
       {isOpen &&<div className='collapse-content'>{content}</div>}
     </div>
  );
