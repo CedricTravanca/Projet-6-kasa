33483bc0e7af8b5f5afeb79a47e755319f258d52
Cedric Travanca <cedric.travanca@gmail.com>
2024-03-18 17:19:18 +0100

    nom et photo host ok , entrian de chercher pour la notation

---
+++ src/Components/Carousel.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+
+const Carousel = ({ pictures }) => {
+    const [currentPictureIndex, setCurrentPictureIndex] = useState(0);
+    //const totalPictures = pictures.length;
+
+
+};
+
+export default Carousel;
--- src/Pages/Logements.js
+++ src/Pages/Logements.js
@@ -1,12 +1,67 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import "../css/Logement.css"
+import { useParams, Navigate } from "react-router-dom";
+import Carousel from "../Components/Carousel";
+import dataHebergements from "../data.json";
+import Collapse from "../Components/Collapse";
+import starActive from "../Assets/star-active.png";
+import starInactive from "../Assets/star-inactive.png";
+
 
 function Logements(){
     const {id} = useParams()
     console.log(id)
-    return(
+    const apartment = dataHebergements.find(apartment => apartment.id === id);
+    console.log(apartment)
 
-    <h1>page des logements</h1>   
+    const tagHebergement = apartment.tags.map((tag) => <span className="tag" key={tag}>{tag}</span>);
+    
+    if (!apartment) {
+        return <Navigate to="*" replace />;
+      }
+
+      function afficherNoteSur5(rating) {
+        // Nombre maximum d'étoiles
+        const maxStars = 5;
+    
+        // Sélectionner l'élément où les étoiles seront affichées
+        const $rating = document.getElementsByClassName("host-rate");
+    
+        // Vider l'élément si déjà rempli
+        $rating.innerHTML = '';
+    
+        // Ajouter les étoiles
+        for (let i = 1; i <= maxStars; i++) {
+            const img = document.createElement('img');
+            img.id = 'star-' + i;
+            img.src = i <= rating ? {starActive} : {starInactive};
+            $rating.appendChild(img);
+            console.log(img)
+        }
+    }
+
+
+    return(
+        <div className="apartment-page">
+            <Carousel />
+            <div className="apartment-container">
+                
+                <div className="apartment-info">
+                    <span className="apartment-title">{apartment?.title}</span>
+                    <span className="apartment-location">{apartment?.location}</span>
+                    <div className="tags">
+                        {tagHebergement}
+                    </div>
+                </div>
+                <div className="host-space">
+                    <div className="host-info">
+                        <span className="host-name">{apartment?.host.name}</span>
+                        <img className="host-picture" src={apartment?.host.picture} alt="Photo de l'Hôte"></img>
+                    </div>    
+                <div className="host-rate">{afficherNoteSur5}</div>
+                </div>
+            </div>
+        </div>   
     )
 }
 
