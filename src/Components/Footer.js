8d166d80d397ab92a55993a89aa8423a36f74078
Cedric Travanca <cedric.travanca@gmail.com>
2024-03-26 17:32:53 +0100

    css media queries accueil

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,19 @@
 import "../css/Footer.css";
 import logoFooter from "../Assets/Logo-footer.png";
 import kasaRights from "../Assets/kasa-rights.png";
+import kasaSmallRights from "../Assets/kasa-small-rights.png";
 
 function Footer() {
     return(
-<div className="footer">
+<div className="footers">
+<div className="footer footer-big">
     <img src={logoFooter} alt="logo kasa" className="logo-kasa-footer"/>
     <img src={kasaRights} alt="droits de kasa" className="kasa-rights"/>
+</div>
+<div className="footer footer-small">
+    <img src={logoFooter} alt="logo kasa" className="logo-kasa-footer"/>
+    <img src={kasaSmallRights} alt="droits de kasa" className="kasa-small-rights"/>
+</div>
 </div>
     )
 }
